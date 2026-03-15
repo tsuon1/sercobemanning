ac9963d67f841e67aeea72749971452a093ead8b
tsuon1 <124439006+tsuon1@users.noreply.github.com>
2026-03-15 09:19:34 +0100

    Set base path for Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
+    base: "/sercobemanning/",
     port: 8080,
     hmr: {
       overlay: false,
